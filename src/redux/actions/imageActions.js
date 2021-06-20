bb890fbe72a9b196708acbffc371c2bd9b2bf731
Siddhartha Sarkar <siddsarkar7@gmail.com>
2021-06-20 14:47:33 +0530

    add pager view dep

--- src/redux/actions/imageActions.js
+++ src/redux/actions/imageActions.js
@@ -1,65 +1,63 @@
 import api from '../../services/api'
 import * as types from '../types'
 
-export const togglePhotoLike = (options = {image: {}}, cb) => (
-    dispatch,
-    getState
-) => {
-    api.toggleImageLike(getState().user.accessToken, {
-        image: options.image
-    }).then(({json, rate}) => {
-        dispatch({type: types.UPDATE_RATE_LIMIT, rate})
-        return cb(json.photo)
-    })
-}
-export const toggleCollection = (
-    options = {collection_id: null, photo_id: null, inCollection: null},
-    cb
-) => (dispatch, getState) => {
-    api.toggleAddToCollection(getState().user.accessToken, {
-        collection_id: options.collection_id,
-        photo_id: options.photo_id,
-        collected: options.inCollection
-    }).then(({json, rate}) => {
-        dispatch({type: types.UPDATE_RATE_LIMIT, rate})
-        return cb({json})
-    })
-}
-
-export const fetchSearch = (
-    options = {query: '', page: 1, search_type: 'photos', per_page: 30}
-) => (dispatch, getState) =>
-    api
-        .getSearchPhotos(getState().user.accessToken, {
-            query: options.query,
-            page: options.page,
-            per_page: options.per_page
+export const togglePhotoLike =
+    (options = {image: {}}, cb) =>
+    (dispatch, getState) => {
+        api.toggleImageLike(getState().user.accessToken, {
+            image: options.image
+        }).then(({json, rate}) => {
+            dispatch({type: types.UPDATE_RATE_LIMIT, rate})
+            return cb(json.photo)
         })
-        .then(({json, rate}) =>
-            dispatch({
-                type:
-                    options.page === 1
-                        ? types.GOT_SEARCH_RESULTS
-                        : types.GOT_MORE_SEARCH_RESULTS,
-                data: json,
-                search_type: options.search_type,
-                query: options.query,
-                page: options.page,
-                per_page: options.per_page,
+    }
+export const toggleCollection =
+    (options = {collection_id: null, photo_id: null, inCollection: null}, cb) =>
+    (dispatch, getState) => {
+        api.toggleAddToCollection(getState().user.accessToken, {
+            collection_id: options.collection_id,
+            photo_id: options.photo_id,
+            collected: options.inCollection
+        }).then(({json, rate}) => {
+            dispatch({type: types.UPDATE_RATE_LIMIT, rate})
+            return cb({json})
+        })
+    }
 
-                rate
-            })
-        )
-        .catch((e) =>
-            dispatch({
-                type: types.GOT_SEARCH_RESULTS,
-                error: e.toString(),
-                search_type: options.search_type,
+export const fetchSearch =
+    (options = {query: '', page: 1, search_type: 'photos', per_page: 30}) =>
+    (dispatch, getState) =>
+        api
+            .getSearchPhotos(getState().user.accessToken, {
                 query: options.query,
                 page: options.page,
                 per_page: options.per_page
             })
-        )
+            .then(({json, rate}) =>
+                dispatch({
+                    type:
+                        options.page === 1
+                            ? types.GOT_SEARCH_RESULTS
+                            : types.GOT_MORE_SEARCH_RESULTS,
+                    data: json,
+                    search_type: options.search_type,
+                    query: options.query,
+                    page: options.page,
+                    per_page: options.per_page,
+
+                    rate
+                })
+            )
+            .catch((e) =>
+                dispatch({
+                    type: types.GOT_SEARCH_RESULTS,
+                    error: e.toString(),
+                    search_type: options.search_type,
+                    query: options.query,
+                    page: options.page,
+                    per_page: options.per_page
+                })
+            )
 
 export function fetchPhotos(
     options = {page: 1, order_by: 'latest', per_page: 10}
